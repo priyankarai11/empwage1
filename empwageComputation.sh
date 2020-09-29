44b2ea88a5729983affa89a6c9b3ad9689b42eab
priyankarai11 <priyankakumbra@gmail.com>
2020-09-29 11:33:01 -0700

    Added:parttime and fulltime functionalities

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "welcome to computation problem"
 ispresent=1
+wageperday=20
+hoursperday=8
 randomCheck=$((RANDOM%2));
 if [ $ispresent -eq $randomCheck ]
 then
@@ -8,3 +10,7 @@ then
 else
 	echo "Employee is absent"
 fi
+
+echo "Calculating daily wage of employee"
+wageperday=$((20*8))
+echo "Daily employee wage $wageperday"
