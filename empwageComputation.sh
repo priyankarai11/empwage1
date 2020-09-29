5d2529154f0e9391a09f86461920857b31794a8f
priyankarai11 <priyankakumbra@gmail.com>
2020-09-29 11:53:54 -0700

    Added:Part time Employee and wage

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "welcome to computation problem"
 ispresent=1
-wageperday=20
+wageperhour=20
 hoursperday=8
+parttimehoursperday=4
 randomCheck=$((RANDOM%2));
 if [ $ispresent -eq $randomCheck ]
 then
@@ -14,3 +15,7 @@ fi
 echo "Calculating daily wage of employee"
 wageperday=$((20*8))
 echo "Daily employee wage $wageperday"
+
+echo "Calculating part time employee and wage"
+ParttimeWageperday=$(($wageperhour*$parttimehoursperday))
+echo "Part time employee Daily wage $ParttimeWageperday"
